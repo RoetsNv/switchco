1c1468dccc74bcd2232a2713101fe54655436b6f
SiebeRoets <sieberoets@hotmail.com>
2022-04-20 21:51:37 +0200

    define outputs

--- src/switchCo.cpp
+++ src/switchCo.cpp
@@ -1,9 +1,10 @@
 #include "switchCo.h"
 
 
-SwitchCo::SwitchCo(byte canID, String friendly_name):
+SwitchCo::SwitchCo(byte canID, String friendly_name,boolean *digitialIO):
     canID(canID),
-    friendly_name(friendly_name)
+    friendly_name(friendly_name),
+    digitalIO(digitalIO)
 {
     setup_inputs();
 
@@ -11,4 +12,27 @@ SwitchCo::SwitchCo(byte canID, String friendly_name):
 //Define input pins
 void setup_inputs(){
     //Built in switch
+    pinMode(15, INPUT);
+    //Switch inputs S1-S6
+    pinMode(33, INPUT);
+    pinMode(26, INPUT);
+    pinMode(27, INPUT);
+    pinMode(13, INPUT);
+    pinMode(4, INPUT);
+    pinMode(16, INPUT);
+}
+
+void SwitchCo::setup_outputs(){
+    //Gpio pins connected to outputs L1->L6
+    int gpio[6]={25,14,12,19,18,17};
+    for(int i=0;i<6;i++){
+        if(this->digitalIO[i]){
+           pinMode(gpio[i], OUTPUT);
+        }
+        else{
+            // configure LED PWM functionalitites
+            ledcSetup(0,this->pwm_freq, this->pwm_res);
+            ledcAttachPin(gpio[i], 0);
+        }  
+    }
 }
--- src/switchCo.h
+++ src/switchCo.h
@@ -3,14 +3,20 @@
 
 class SwitchCo {
     private:
+        //module specific
         byte canID;
         String friendly_name;
         String hw_version;
         byte data_buffer[8];
+        //output related
+        boolean *digitalIO; // which outputs are Digital IO (1) or analog IO (0)
+        const int pwm_freq=10000;
+        const int pwm_res=8; //resolution of pwm outputs
     public:
         
-        SwitchCo(byte canID,String friendly_name);
+        SwitchCo(byte canID,String friendly_name,boolean *digitialIO);
         void init();
+        void setup_outputs();
 
 
 };
